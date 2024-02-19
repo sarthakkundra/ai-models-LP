86f67a02c67284978ad5b166306600b7c655d5dc
Sarthak kundra <sarthakkundra21@gmail.com>
2024-02-19 22:58:22 +0530

    added resizable libraryy

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -3,14 +3,23 @@ import TaskList from "./taskList";
 import Models from "./models";
 import Clock from "./clock";
 import Footer from "./footer";
+import { PanelGroup, Panel, PanelResizeHandle } from "react-resizable-panels";
 
 const Layout = () => {
 	return (
 		<section className='max-h-[100vh] overflow-hidden'>
 			<div className='flex'>
-				<TaskList />
-				<Models />
-				<Clock />
+				<PanelGroup autoSaveId='example' direction='horizontal'>
+					<Panel>
+						<TaskList />
+					</Panel>
+					<PanelResizeHandle />
+					<Panel>
+						<Models />
+					</Panel>
+					<PanelResizeHandle />
+					<Clock />
+				</PanelGroup>
 			</div>
 			<Footer />
 		</section>
