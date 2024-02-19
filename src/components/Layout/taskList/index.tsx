ea4227712cfccd2b15e4f14489c0dc827c21c9d8
Sarthak kundra <sarthakkundra21@gmail.com>
2024-02-20 00:36:24 +0530

    fixed margin bottom and clock bg

--- src/components/Layout/taskList/index.tsx
+++ src/components/Layout/taskList/index.tsx
@@ -4,7 +4,7 @@ import { tasks } from "../../../data";
 
 const TaskList = () => {
 	return (
-		<section className='bg-[#26252a] min-w-[285px] p-8 h-[70vh] overflow-auto resize-x rounded-xl'>
+		<section className='bg-[#26252a] min-w-[285px] p-8 h-[69vh] overflow-auto resize-x rounded-xl'>
 			<h3 className='text-white font-bold text-xl mb-6'>Today's Tasks</h3>
 			{tasks?.map((task) => (
 				<TaskItem task={task} shouldStrikethrough />
---
+++ src/components/modal/index.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import styles from "./index.module.css"; // Import CSS for custom animations
+
+interface IComponentProps {
+	isOpen: boolean;
+	setIsOpen: Function;
+	children?: any;
+}
+
+const Modal: React.FC<IComponentProps> = ({ isOpen, setIsOpen, children }) => {
+	if (!isOpen) return null;
+
+	return (
+		<div
+			className='fixed inset-0 bg-gray-600 bg-opacity-50 overflow-y-auto h-full w-full'
+			onClick={() => setIsOpen(false)}>
+			<div className={`relative top-20 mx-auto p-5 border w-96 shadow-lg rounded-md bg-white ${styles.animateBounce}`}>
+				<div onClick={(e) => e.stopPropagation()}>
+					{children}
+          <h3 className="text-black text-lg">Custom Modal</h3>
+					<button
+						className='absolute top-0 right-0 m-2'
+						onClick={() => setIsOpen(false)}>
+						Close
+					</button>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Modal;
